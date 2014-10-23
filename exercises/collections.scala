7f1f943aba1176d1fcec71b6af7d537b3ef193d6
Mika Tuominen <tuomimi@mikat-mac.local>
2014-10-24 01:00:36 +0300

    collection edit

--- exercises/collections.scala
+++ exercises/collections.scala
@@ -1,128 +1,127 @@
 object Functions {
 
-    // convert a string to upper case, e.g. John => JOHN
-    def upper(s:String) = ???
+  // convert a string to upper case, e.g. John => JOHN
+  def upper(s:String) = s.toUpperCase
 
-    // convert a list of strings to list of upper case strings
-    def listToUpper(list:List[String]) = ???
+  // convert a list of strings to list of upper case strings
+  def listToUpper(list:List[String]) = list.map(x => x.toUpperCase)
+
+  // return the length of a string
+  def length(str: String):Int = str.length
+
+  // filter names that are less than four characters long
+  def filterShortNames(names:List[String]) = names.filter(x => x.length > 3)
+
+  // filter names that are less than four characters long
+  // and turn remaining names to uppercase
+  def filterShortAndTurnToUppercase(names:List[String]) = names.filter(x => x.length > 3).map(x => x.toUpperCase)
+
+  // turn a string to a list of characters
+  def characters(str: String):List[Char] = str.toList
+
+  // turn a list of strings to a list of list of characters
+  def toCharacterLists(list:List[String]):List[List[Char]] = list.map(x => x.toList)
+
+  // turn a list of strings to a list of characters
+  // flatMap[A,B](xs:List[A], f: (A => List[B]):List[B])
+  // nestedNumbers.flatMap(x => x.map(_ * 2))
+  def listOfAllCharacters(list:List[String]):List[Char] = list.flatMap(x => x.toList)
+
+  // return the first string in a list, or if list is empty, return "nada"
+  def firstElementInListOrNada(list:List[String]) = list.headOption.getOrElse("nada")
+
+  // find the longest string in given list of strings, or "nada" if empty
+  // and reverse the result before returning it
+  def longestNameInList(names:List[String]) = if (names.isEmpty) "adan" else names.sorted.reverse.head.reverse
+
+  // Join list `xs` of string with separator string `sep` into a string
+  def joinWithSep(xs: List[String], sep: String): String = xs.mkString(sep)
+
+  // Take sums of lists and return those with sum greater than 20
+  //
+  // sumOfLists20(List(List(1,2,3), List(2,10,9))) == List(21)
+  def sumOfLists20(xs: List[List[Int]]): List[Int] = xs.filter(x => x.sum > 20).map(x => x.sum)
 
 
-    // return the length of a string
-    def length(str: String):Int = ???
+  // Return only cards of suit "heart"
+  //
+  // onlyHearts(List(("diamond", 4), ("heart", 9), ("clubs", 10))) ==  List(("heart", 9))
+  def onlyHearts(xs: List[(String, Int)]): List[(String, Int)] = ???
 
-    // filter names that are less than four characters long
-    def filterShortNames(names:List[String]) = ???
+  def main(args: Array[String]) {
 
-    // filter names that are less than four characters long
-    // and turn remaining names to uppercase
-    def filterShortAndTurnToUppercase(names:List[String]) = ???
+    println(s"${Console.GREEN}Running ${tests.size} tests:")
 
-
-    // turn a string to a list of characters
-    def characters(str: String):List[Char] = ???
-
-    // turn a list of strings to a list of list of characters
-    def toCharacterLists(list:List[String]):List[List[Char]] = ???
-
-    // turn a list of strings to a list of characters
-    def listOfAllCharacters(list:List[String]):List[Char] = ???
-
-
-    // return the first string in a list, or if list is empty, return "nada"
-    def firstElementInListOrNada(list:List[String]) = ???
-
-    // find the longest string in given list of strings, or "nada" if empty
-    // and reverse the result before returning it
-    def longestNameInList(names:List[String]) = ???
-
-    // Join list `xs` of string with separator string `sep` into a string
-    def joinWithSep(xs: List[String], sep: String): String = ???
-
-    // Take sums of lists and return those with sum greater than 20
-    //
-    // sumOfLists20(List(List(1,2,3), List(2,10,9))) == List(21)
-    def sumOfLists20(xs: List[List[Int]]): List[Int] = ???
-
-
-    // Return only cards of suit "heart"
-    //
-    // onlyHearts(List(("diamond", 4), ("heart", 9), ("clubs", 10))) ==  List(("heart", 9))
-    def onlyHearts(xs: List[(String, Int)]): List[(String, Int)] = ???
-
-    def main(args: Array[String]) {
-        
-        println(s"${Console.GREEN}Running ${tests.size} tests:")
-
-        tests.foreach { (fixture) =>
-            val name = fixture._1
-            val test = fixture._2
-            try {
-                print(s"${Console.GREEN}\t- '$name': ")
-                test()
-                println("PASS!")
-            } catch {
-                case e:Error => {
-                    println(Console.RED + e.getMessage)
-                }
-            }
-
+    tests.foreach { (fixture) =>
+      val name = fixture._1
+      val test = fixture._2
+      try {
+        print(s"${Console.GREEN}\t- '$name': ")
+        test()
+        println("PASS!")
+      } catch {
+        case e:Error => {
+          println(Console.RED + e.getMessage)
         }
+      }
+
     }
+  }
 
-    def verify(expected: Any, actual: Any) =
-        assert(expected == actual, s"Expected: $expected, actual: $actual")
+  def verify(expected: Any, actual: Any) =
+    assert(expected == actual, s"Expected: $expected, actual: $actual")
 
-    val tests = List[(String, Function0[Unit])](
-        ("upper", () => {
-            verify("FOO", upper("foo"))
-            verify("BAR", upper("bar"))
-            verify("BAZ", upper("baz"))
-            verify("12", upper("12"))
-        }),
-        ("list to uppercase", () => {
-            verify(List("FOO", "BAR"), listToUpper(List("foo", "bar")))
-        }),
-        ("length", () => {
-            verify(3, length("foo"))
-            verify(9, length("foobarbaz"))
-        }),
-        ("filter short names", () => {
-            verify(List("John", "Suzy"), filterShortNames(List("Joe", "John", "Fae", "Suzy")))
-        }),
-        ("filter short names and convert to uppercase", () => {
-            verify(List("JACK", "SUZY"), filterShortAndTurnToUppercase(List("Joe", "Jack", "Fae", "Suzy")))
-        }),
-        ("turn string to a list of characters", () => {
-            verify(List('a', 'b', 'c'), characters("abc"))
-        }),
-        ("list of strings to a list of list of characters", () => {
-            verify(List(List('f', 'o', 'o'), List('b', 'a', 'r'), List('b', 'a', 'z')), toCharacterLists(List("foo", "bar", "baz")))
-        }),
-        ("list of strings to a list of characters", () => {
-            verify(List('f', 'o', 'o', 'b', 'a', 'r', 'b', 'a', 'z'), listOfAllCharacters(List("foo", "bar", "baz")))
-        }),
-        ("get first element in list or 'nada'", () => {
-            verify("foo", firstElementInListOrNada(List("foo", "bar")))
-            verify("bar", firstElementInListOrNada(List("bar")))
-            verify("nada", firstElementInListOrNada(List()))
-        }),
-        ("find the longest name in the list", () => {
-            verify("drahciR", longestNameInList(List("Jack", "John", "Jill", "Richard", "Jim")))
-            verify("adan", longestNameInList(List()))
-        }),
+  val tests = List[(String, Function0[Unit])](
+    ("upper", () => {
+      verify("FOO", upper("foo"))
+      verify("BAR", upper("bar"))
+      verify("BAZ", upper("baz"))
+      verify("12", upper("12"))
+    }),
+    ("list to uppercase", () => {
+      verify(List("FOO", "BAR"), listToUpper(List("foo", "bar")))
+    }),
+    ("length", () => {
+      verify(3, length("foo"))
+      verify(9, length("foobarbaz"))
+    }),
+    ("filter short names", () => {
+      verify(List("John", "Suzy"), filterShortNames(List("Joe", "John", "Fae", "Suzy")))
+    }),
+    ("filter short names and convert to uppercase", () => {
+      verify(List("JACK", "SUZY"), filterShortAndTurnToUppercase(List("Joe", "Jack", "Fae", "Suzy")))
+    }),
+    ("turn string to a list of characters", () => {
+      verify(List('a', 'b', 'c'), characters("abc"))
+    }),
+    ("list of strings to a list of list of characters", () => {
+      verify(List(List('f', 'o', 'o'), List('b', 'a', 'r'), List('b', 'a', 'z')), toCharacterLists(List("foo", "bar", "baz")))
+    }),
+    ("list of strings to a list of characters", () => {
+      verify(List('f', 'o', 'o', 'b', 'a', 'r', 'b', 'a', 'z'), listOfAllCharacters(List("foo", "bar", "baz")))
+    }),
+    ("get first element in list or 'nada'", () => {
+      verify("foo", firstElementInListOrNada(List("foo", "bar")))
+      verify("bar", firstElementInListOrNada(List("bar")))
+      verify("nada", firstElementInListOrNada(List()))
+    }),
+    ("find the longest name in the list", () => {
+      verify("drahciR", longestNameInList(List("Jack", "John", "Jill", "Richard", "Jim")))
+      verify("adan", longestNameInList(List()))
+    }),
 
-        ("join with separator", () => {
-            verify("hello, world!", joinWithSep(List("hello", "world!"), ", "))
-            verify("This, that, and the other", joinWithSep(List("This", "that", "and the other"), ", "))
-        }),
+    ("join with separator", () => {
+      verify("hello, world!", joinWithSep(List("hello", "world!"), ", "))
+      verify("This, that, and the other", joinWithSep(List("This", "that", "and the other"), ", "))
+    }),
 
-        ("sum of lists greater than 20", () => {
-            verify(List(30, 40), sumOfLists20(List(List(1,10), List(5,15), List(5, 20, 5), List(10,10,5,5,3,7))))
-        }),
+    ("sum of lists greater than 20", () => {
+      verify(List(30, 40), sumOfLists20(List(List(1,10), List(5,15), List(5, 20, 5), List(10,10,5,5,3,7))))
+    }),
 
-        ("cards with hearts", () => {
-            verify(List(("heart", 10), ("heart", 4)), onlyHearts(List(("heart", 10), ("clubs", 8), ("heart", 4))))
-        })
-    )
+    ("cards with hearts", () => {
+      verify(List(("heart", 10), ("heart", 4)), onlyHearts(List(("heart", 10), ("clubs", 8), ("heart", 4))))
+    })
+  )
 
 }
--- exercises/functions.scala
+++ exercises/functions.scala
@@ -1,89 +1,90 @@
 object Functions {
 
-    // adds two integers together
-    def plus(x: Int, y:Int):Int = ???
-    
-    // salutes the given name, e.g. Hello, User
-    def salutation(name: String):String = ???
+  // adds two integers together
+  def plus(x: Int, y:Int):Int = x + y
 
-    // counts the number of characters in a given string
-    def characters(name: String):Int = ???
+  // salutes the given name, e.g. Hello, User
+  def salutation(name: String):String = "Hello, " + name
 
-    // returns the base x to the power y, i.e. x^y
-    def pow(x: Int, y: Int) = ???
+  // counts the number of characters in a given string
+  def characters(name: String):Int = name.length()
 
-    /** Salutes the user according to the time of day given in isDayTime
-      * During daytime, salutes with "Hello". During nighttime, salutes with "Good night"
-      */
-    def dayOrNightSalutation(name:String, isDayTime: Boolean) = ???
-
-    // return first two names in a given list
-    def firstTwo(names: List[String]) = ???
-
-    // return the last name in a given list
-    def lastName(names: List[String]) = ???
-
-    // combines two lists
-    def combine(names: List[String], letters: List[String]) = ???
+  // returns the base x to the power y, i.e. x^y
+  def pow(x: Int, y: Int) = math.pow(x, y)
 
 
+  /** Salutes the user according to the time of day given in isDayTime
+    * During daytime, salutes with "Hello". During nighttime, salutes with "Good night"
+    */
+  def dayOrNightSalutation(name:String, isDayTime: Boolean) = if (isDayTime) "Hello, " + name else "Good night, " + name
 
-    def main(args: Array[String]) {
-        
-        println(s"${Console.GREEN}Running ${tests.size} tests:")
+  // return first two names in a given list
+  def firstTwo(names: List[String]) = names.take(2)
 
-        tests.foreach { (fixture) =>
-            val name = fixture._1
-            val test = fixture._2
-            try {
-                print(s"${Console.GREEN}\t- '$name': ")
-                test()
-                println("PASS!")
-            } catch {
-                case e:Error => {
-                    println(Console.RED + e.getMessage)
-                }
-            }
+  // return the last name in a given list
+  def lastName(names: List[String]) = names.last
 
+  // combines two lists
+  def combine(names: List[String], letters: List[String]) = names ++ letters
+
+
+
+  def main(args: Array[String]) {
+
+    println(s"${Console.GREEN}Running ${tests.size} tests:")
+
+    tests.foreach { (fixture) =>
+      val name = fixture._1
+      val test = fixture._2
+      try {
+        print(s"${Console.GREEN}\t- '$name': ")
+        test()
+        println("PASS!")
+      } catch {
+        case e:Error => {
+          println(Console.RED + e.getMessage)
         }
+      }
+
     }
+  }
 
-    def verify(expected: Any, actual: Any) =
-        assert(expected == actual, s"Expected: $expected, actual: $actual")
+  def verify(expected: Any, actual: Any) =
+    assert(expected == actual, s"Expected: $expected, actual: $actual")
 
-    val tests = List[(String, Function0[Unit])](
-        ("plus", () => {
-            verify(6, plus(2, 4))
-            verify(4, plus(2, 2))
-        }),
-        ("salutation", () => {
-            verify("Hello, World", salutation("World"))
-            verify("Hello, Mark" , salutation("Mark"))
-        }),
-        ("character count", () => {
-            verify(6, characters("foobar"))
-            verify(22, characters("functional programming"))
-        }),
-        ("power", () => {
-            verify(216, pow(6, 3))
-            verify(32, pow(2, 5))
-        }),
-        ("time-of-day-aware salutation", () => {
-            verify("Hello, Mark", dayOrNightSalutation("Mark", true))
-            verify("Good night, Mark", dayOrNightSalutation("Mark", false))
-            verify("Good night, World", dayOrNightSalutation("World", false))
-        }),
-        ("get first two names in list", () => {
-            verify(List("Jill", "Jack"), firstTwo(participants))
-        }),
-        ("get last two names in list", () => {
-            verify("Mary", lastName(participants))
-        }),
-        ("combine two lists", () => {
-            verify(List("Jill", "Jack", "John", "George", "Mary", "a", "b", "c"), combine(participants, letters))
-        })
-    )
+  val tests = List[(String, Function0[Unit])](
+    ("plus", () => {
+      verify(6, plus(2, 4))
+      verify(4, plus(2, 2))
+    }),
+    ("salutation", () => {
+      verify("Hello, World", salutation("World"))
+      verify("Hello, Mark" , salutation("Mark"))
+    }),
+    ("character count", () => {
+      verify(6, characters("foobar"))
+      verify(22, characters("functional programming"))
+    }),
+    ("power", () => {
+      verify(216, pow(6, 3))
+      verify(32, pow(2, 5))
+    }),
+    ("time-of-day-aware salutation", () => {
+      verify("Hello, Mark", dayOrNightSalutation("Mark", true))
+      verify("Good night, Mark", dayOrNightSalutation("Mark", false))
+      verify("Good night, World", dayOrNightSalutation("World", false))
+    }),
+    ("get first two names in list", () => {
+      verify(List("Jill", "Jack"), firstTwo(participants))
+    }),
+    ("get last two names in list", () => {
+      verify("Mary", lastName(participants))
+    }),
+    ("combine two lists", () => {
+      verify(List("Jill", "Jack", "John", "George", "Mary", "a", "b", "c"), combine(participants, letters))
+    })
+  )
 
-    val participants = List("Jill", "Jack", "John", "George", "Mary")
-    val letters = List("a", "b", "c")
+  val participants = List("Jill", "Jack", "John", "George", "Mary")
+  val letters = List("a", "b", "c")
 }
